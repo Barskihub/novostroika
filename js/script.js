7cf68f4574f96e0f763802fee207cc2c21fd1eed
Arthur Barski <barski.10877716@gmail.com>
2020-01-27 12:10:24 +0300

    slider work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Burger Actions
 const burgerMotion = () => {
     const burger = document.querySelector(".header__btn");
 
@@ -12,3 +13,78 @@ const burgerMotion = () => {
 };
 
 burgerMotion();
+
+//Slider
+//slider=slider-h>line=slider-h__list>slide=slider-h__item
+const sliderH = () => {
+    let sliderHItems = document.querySelectorAll(".slider-h__item");
+    let sliderHList = document.querySelector(".slider-h__list");
+    let sliderW = document.querySelector(".slider-w");
+
+    const btnHnext = document.querySelector(".slider-h__btn-next");
+    const btnHprev = document.querySelector(".slider-h__btn-prev");
+    const btnWprev = document.querySelector(".slider-w__btn-prev");
+    const btnWnext = document.querySelector(".slider-w__btn-next");
+    let step = 0;
+    let backStep = 0;
+
+    const sliderMotionBack = () => {
+        step = step + 91;
+        sliderHList.style.top = step+'px';
+
+        if (step === 91) {
+            step = 0;
+            sliderHList.style.top = step +'px';
+        }
+    };
+
+    const sliderMotionForward = () => {
+        step = step - 91;
+        sliderHList.style.top = step+'px';
+
+        if (step === -364) {
+            step = 0;
+            sliderHList.style.top = step +'px';
+        }
+    };
+
+    const sliderMotionWBack = () => {
+        sliderW.style.backgroundImage = `url("./img/h${backStep = backStep - 1}.png")`;
+
+        if (backStep === -1) {
+            backStep = 0;
+            sliderW.style.backgroundImage = `url("./img/h0.png")`
+        }
+    };
+
+    const sliderMotionWForward = () => {
+        sliderW.style.backgroundImage = `url("./img/h${backStep = backStep + 1}.png")`;
+
+        if (backStep === 6) {
+            backStep = 0;
+            sliderW.style.backgroundImage = `url("./img/h0.png")`
+        }
+    };
+
+    btnHprev.addEventListener("click", sliderMotionBack);
+    btnHnext.addEventListener("click", sliderMotionForward);
+    btnWprev.addEventListener("click", sliderMotionWBack);
+    btnWnext.addEventListener("click", sliderMotionWForward);
+
+
+    for (let i = 0; i < sliderHItems.length; i++) {
+        sliderHItems[i].addEventListener("click", () => {
+            sliderHItems[i].classList.toggle("active");
+
+            if (sliderHItems[i].classList.contains('active')) {
+                sliderW.style.backgroundImage = `url("./img/h${i}.png")`;
+                sliderHItems[i].classList.toggle("active");
+            }
+        });
+    }
+    console.log(sliderHItems);
+};
+
+sliderH();
+
+
